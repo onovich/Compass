63d96f921aff457dea25ed89c65a84bf207f7a50
onovich <hongjianan@4399inc.com>
2023-06-13 23:31:33 +0800

    <WIP> 根据 Polygon Collider 检测障碍物

--- Assets/com.mortise.compass/Sample/GridEditor.cs
+++ Assets/com.mortise.compass/Sample/GridEditor.cs
@@ -17,6 +17,9 @@ namespace MortiseFrame.Compass.Sample {
         [Header("不可通行颜色")] public Color cellColor_unwalkable = Color.red;
         [Header("边框颜色")] public Color cellColor_border = Color.black;
 
+        Vector2 overLapPos = Vector2.zero;
+        bool overLapDone = false;
+
         [ContextMenu("生成网格")]
         void SaveAsset() {
 
@@ -31,46 +34,68 @@ namespace MortiseFrame.Compass.Sample {
             model.tm.width = gridSize.x;
             model.tm.height = gridSize.y;
 
-            var obstacles = GetObstacleArray();
-            for (int i = 0; i < obstacles.Length; i++) {
-                var obstacle = obstacles[i];
-                var x = (int)obstacle.X;
-                var y = (int)obstacle.Y;
-                grid[x, y].Init(x, y, obstacle.Walkable);
-                Debug.Log($"添加障碍物:x={x},y={y}");
+            overLapDone = false;
+
+            var polygons = GetPolygonsArray();
+            for (int i = 0; i < polygons.Length; i++) {
+
+                var polygon = polygons[i];
+                for (int x = 0; x < gridSize.x; x++) {
+                    for (int y = 0; y < gridSize.y; y++) {
+                        if (IsPointInPolygon(x, y, polygon)) {
+                            grid[x, y].Init(x, y, false);
+                            Debug.Log($"添加障碍物:x={x},y={y}");
+                        }
+                    }
+                }
             }
+
+            overLapDone = true;
 
             EditorUtility.SetDirty(this);
             EditorUtility.SetDirty(model);
 
         }
 
-        public Node2D[] GetObstacleArray() {
+        bool IsPointInPolygon(int x, int y, Vector2[] polygon) {
+            bool isInside = false;
+            var point = new Vector2(x * cellSize.x + transform.position.x, y * cellSize.y + transform.position.y);
+            for (int i = 0, j = polygon.Length - 1; i < polygon.Length; j = i++) {
+                if (((polygon[i].y > point.y) != (polygon[j].y > point.y)) &&
+                    (point.x < (polygon[j].x - polygon[i].x) * (point.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x)) {
+                    isInside = !isInside;
+                }
+            }
+            return isInside;
+        }
+
+        public Vector2[][] GetPolygonsArray() {
             var group = transform.Find("editor_obstacle_group");
             var goes = group.transform.GetComponentsInChildren<ObstacleEditor>();
             if (goes == null || goes.Length == 0) {
                 return null;
             }
-
-            var arr = new Node2D[goes.Length];
+            var arr = new Vector2[goes.Length][];
             for (int i = 0; i < goes.Length; i++) {
                 var go = goes[i];
-                var node = go.GetNode();
-                arr[i] = node;
+                var collider = go.GetComponent<PolygonCollider2D>();
+                var points = collider.points;
+                for (int j = 0; j < points.Length; j++) {
+                    var offset = collider.offset;
+                    points[j] = new Vector2(points[j].x + go.transform.position.x, points[j].y + go.transform.position.y) + offset;
+                }
+                arr[i] = points;
             }
             return arr;
         }
 
         void OnDrawGizmos() {
-            // var gridOffset = new Vector2((float)gridSize.x * cellSize.x / 2f, (float)gridSize.y * cellSize.y / 2f);
-            // var boxOffset = new Vector2(cellSize.x / 2f, cellSize.y / 2f);
             if (model == null || model.tm == null || model.tm.grid == null) {
                 return;
             }
             for (int x = 0; x < gridSize.x; x++) {
                 for (int y = 0; y < gridSize.y; y++) {
                     Vector3 pos = new Vector3(x * cellSize.x, y * cellSize.y, 0);
-                    // pos += transform.position - (Vector3)gridOffset + (Vector3)boxOffset;
                     pos += transform.position;
                     if (model.tm.grid[x, y].Walkable) {
                         Gizmos.color = cellColor_walkable;
@@ -78,11 +103,14 @@ namespace MortiseFrame.Compass.Sample {
                         Gizmos.color = cellColor_unwalkable;
                         Gizmos.DrawCube(pos, cellSize);
                     }
-                    // Gizmos.DrawCube(pos, cellSize);
-                    // Gizmos.color = cellColor_border;
                     Gizmos.DrawWireCube(pos, cellSize);
                 }
             }
+            if (!overLapDone) {
+                return;
+            }
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(overLapPos, cellSize);
         }
 
     }
