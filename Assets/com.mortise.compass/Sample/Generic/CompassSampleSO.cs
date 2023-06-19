3b41cdc6de9149126c093f34e7c9d15ee5503ed7
onovich <hongjianan@4399inc.com>
2023-06-19 19:31:31 +0800

    <mics> remove: SampleSO 创建方式

--- Assets/com.mortise.compass/Sample/Generic/CompassSampleSO.cs
+++ Assets/com.mortise.compass/Sample/Generic/CompassSampleSO.cs
@@ -3,7 +3,6 @@ using MortiseFrame.Modifier.Toaster.Generic;
 
 namespace MortiseFrame.Compass.Sample {
 
-    [CreateAssetMenu(fileName = "ToasterSampleSO", menuName = "MortiseFrame/Compass/Sample/ToasterSampleSO")]
     public class CompassSampleSO : ScriptableObject {
 
         [SerializeField] public ToasterGridTM tm;
