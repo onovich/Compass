18b29a294e4fb9e6c65017ae1e58dcbb06ab5a6c
onovich <hongjianan@4399inc.com>
2023-06-25 16:45:30 +0800

    Revert "<feature> modify: 修正边界范围判定"
    This reverts commit 22156a99b54e5df55336e2febd2cd6b0cc244499.

--- Assets/com.mortise.compass/Runtime/Util/MathUtil.cs
+++ Assets/com.mortise.compass/Runtime/Util/MathUtil.cs
@@ -7,27 +7,15 @@ namespace MortiseFrame.Compass {
 
         public static Node2D Pos2Node(Vector3 pos, int mpu, Vector2 localOffset, Map2D map) {
 
-            var x = Mathf.FloorToInt(pos.x - localOffset.x) * mpu;
-            var y = Mathf.FloorToInt(pos.y - localOffset.y) * mpu;
-            var index = new Vector2Int(x, y);
+            var x = Mathf.RoundToInt((pos.x - localOffset.x) * mpu);
+            var y = Mathf.RoundToInt((pos.y - localOffset.y) * mpu);
 
-            // Index2CenterPos
-            if (index.x < 0 || index.x >= map.Width || index.y < 0 || index.y >= map.Height) {
-                Debug.LogError($"out of range: x={x}, y={index.y}, width={map.Width}, height={map.Height}, localOffset = {localOffset}");
+            var node = map.Nodes[x, y];
+            if (node == null) {
+                Debug.LogError($"node is null: {x}, {y}");
             }
 
-            var node = map.Nodes[index.x, index.y];
-
-            var nodeX = Mathf.RoundToInt(node.X);
-            var nodeY = Mathf.RoundToInt(node.Y);
-
-            var fixedNode = map.Nodes[nodeX, nodeY];
-
-            if (fixedNode == null) {
-                Debug.LogError($"node is null: {index.x}, {index.y}");
-            }
-
-            return fixedNode;
+            return node;
 
         }
 
